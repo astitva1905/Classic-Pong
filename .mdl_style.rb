0e073abd1d1b5e5b6e7bf557ae291f1fa852182c
Astitva Sehgal <69433630+astitva1905@users.noreply.github.com>
2023-09-28 19:55:13 +0530

    Update .mdl_style.rb

--- .mdl_style.rb
+++ .mdl_style.rb
@@ -1,5 +1,5 @@
+git_recurse true
 all
-# our changelog does this, by design
-exclude_rule 'MD024'
-# default in next version, remove then
-rule 'MD007', :indent => 3
+rule 'MD013', :ignore_code_blocks => true
+rule 'MD013', :tables => false
+rule 'MD046', :indented
