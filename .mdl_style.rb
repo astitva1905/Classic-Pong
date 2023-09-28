9cf3d2cfdb39d0534c4f7b288f6ab521ed53f910
Astitva Sehgal <69433630+astitva1905@users.noreply.github.com>
2023-09-28 20:04:55 +0530

    Update .mdl_style.rb

--- .mdl_style.rb
+++ .mdl_style.rb
@@ -3,3 +3,6 @@ all
 exclude_rule 'MD024'
 # default in next version, remove then
 rule 'MD007', :indent => 3
+rule 'MD013', :ignore_code_blocks => true
+rule 'MD013', :tables => false
+rule 'MD046', :indented
